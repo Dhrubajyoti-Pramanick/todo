2efde8854ea4e06f0256edc179c9569579f40480
Dhrubajyoti-Pramanick <dhrubajyoti@itobuz.com>
2023-02-09 13:08:06 +0530

    another commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let val1=document.getElementById("inputBox");
-var arr1=[];
-var arr2;
+let arr1=[];
+let arr2,i;
 
 document.getElementById('add').onclick = function(){
     if (document.querySelector('#inputBox').value.length == 0) {
-        alert("Enter a task")
+        alert("Enter a task");
 
     }
     else {
@@ -13,79 +13,60 @@ document.getElementById('add').onclick = function(){
                 <button id="tick">tick</button>
                 <button id="trash">trash</button>
             </div><br/>`;
-    event.preventDefault();
-    document.getElementById('location').innerHTML+=a;
-    document.getElementById('inputBox').value="";
+        event.preventDefault();
+        document.getElementById('location').innerHTML+=a;
+        document.getElementById('inputBox').value="";
 
-    arr1 = document.querySelectorAll('#tick');
-    for (var i=0;i< arr1.length;i++) {
-        
-        arr1[i].onclick=function(){
-            this.parentNode.style.textDecoration = "line-through";
-            event.preventDefault();
+        arr1 = document.querySelectorAll('#tick');
+        for (i = 0; i < arr1.length; i++) {
+            arr1[i].onclick=function(){
+                this.parentNode.style.textDecoration = "line-through";
+                event.preventDefault();
+            }
         }
-    }
-    arr2 = document.querySelectorAll('#trash');
-
-    for (var i=0;i< arr2.length;i++) {
-        
-        arr2[i].onclick=function(){
-            event.preventDefault();
-            this.parentNode.remove();
+        arr2 = document.querySelectorAll('#trash');
+        for (i = 0; i < arr2.length; i++) {
+            arr2[i].onclick=function(){
+                this.parentNode.remove();
             }
         }
     }
 }
 
 document.getElementById('completed').onclick= function(){
-    var arr3 = document.querySelectorAll("#tick");
-    for (var i = 0; i < arr3.length; i++) {
+    for (i = 0; i < arr1.length; i++) {
         event.preventDefault();
            if(arr1[i].parentNode.style.textDecoration){
-           arr1[i].parentNode.style.display="block";
+                arr1[i].parentNode.style.display="flex";
            }
            else {
-            arr1[i].parentNode.style.display="none";
+                arr1[i].parentNode.style.display="none";
            }
         }
 }
 document.getElementById('active').onclick= function(){
-    var arr1 = document.querySelectorAll("#tick");
-    for (var i = 0; i < arr1.length; i++) {
-        event.preventDefault();
-           if(arr1[i].parentNode.style.textDecoration){
-           arr1[i].parentNode.style.display="none"
+    for (i = 0; i < arr1.length; i++) {
+           event.preventDefault();
+            if(arr1[i].parentNode.style.textDecoration){
+                arr1[i].parentNode.style.display="none";
            }
            else {
-            arr1[i].parentNode.style.display="flex"
+                arr1[i].parentNode.style.display="flex";
            }
         }
 }
 document.getElementById('clear').onclick= function(){
-    var arr3 = document.querySelectorAll("#tick");
-    for (var i = 0; i < arr3.length; i++) {
-        event.preventDefault();
-        //    if(arr1[i].parentNode.style.textDecoration){
-        //    arr1[i].parentNode.style.display="block";
-        //    }
-        //    else {
+    for (i = 0; i < arr1.length; i++) {
+            event.preventDefault();
             arr1[i].parentNode.style.display="none";
-        //    }
         }
 }
 document.getElementById('all').onclick= function(){
-    var arr3 = document.querySelectorAll("#tick");
-    for (var i = 0; i < arr3.length; i++) {
-        event.preventDefault();
-        //    if(arr1[i].parentNode.style.textDecoration){
-        //    arr1[i].parentNode.style.display="block";
-        //    }
-        //    else {
-            arr1[i].parentNode.style.display="block";
-        //    }
+    for (i = 0; i < arr1.length; i++) {
+            event.preventDefault();
+            arr1[i].parentNode.style.display="flex";
         }
 }
 
-// document.getElementById("d").addEventListener("")
 
 
